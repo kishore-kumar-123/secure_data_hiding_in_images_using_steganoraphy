f4882ca4a4e578bc37f631feafc57052f19bf44b
kishore-kumar-123 <iamkishorek123@gmail.com>
2025-02-17 22:52:05 +0530

    first commit

---
+++ secure_data_hiding_in_images_using_steganoraphy.py
@@ -0,0 +1,43 @@
+import cv2
+import os
+import string
+
+img = cv2.imread(r"C:\Users\Kavin kumar\Downloads\secure_data_hiding_in_images_using_steganoraphy-master\Image.jpg")
+msg = input("Secret Code To Unlock:")
+password = input("Enter The passkey:")
+
+d = {}
+c = {}
+
+for i in range(255):
+    d[chr(i)] = i
+    c[i] = chr(i)
+
+m = 0
+n = 0
+z = 0
+
+for i in range(len(msg)):
+    img[n, m, z] = d[msg[i]]
+    n = n + 1
+    m = m + 1
+    z = (z + 1) % 3
+
+cv2.imwrite("encryptedImage.jpg", img)
+os.system("start encryptedImage.jpg")  # Use 'start' to open the image on Windows
+
+message = ""
+n = 0
+m = 0
+z = 0
+
+pas = input("Enter passcode for Decryption")
+if password == pas:
+    for i in range(len(msg)):
+        message = message + c[img[n, m, z]]
+        n = n + 1
+        m = m + 1
+        z = (z + 1) % 3
+    print("Decryption message:", message)
+else:
+    print("YOU ARE NOT auth")
